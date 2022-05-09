b490c3f6e8be6358739221591c3344e92ce46aa3
Andres M. Prieto <andresprieto2703@gmail.com>
2022-05-09 02:16:25 -0500

    Roting & tokenization
    Adding roting paths, adding tokenization for request under auth

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const cors = require('cors');
 const path = require('path');
 const morgan = require('morgan');
+const router = require('./routes');
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(morgan('dev'));
+
+app.use(cors());
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/api', router);
 
 app.set('port', process.env.PORT || 3000);
 
