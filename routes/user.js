800397cd61654b2e632c99b59632f5bccf3d8c07
Andres M. Prieto <andresprieto2703@gmail.com>
2022-05-13 20:26:18 -0500

    Router
    Adding some routes

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
-const routerx = require('express-promise-router');
-const userController = require('../controllers/userController');
-const auth = require('../middlewares/auth');
+const routerx = require("express-promise-router");
+const userController = require("../controllers/userController");
+const auth = require("../middlewares/auth");
 // Instance
 const router = routerx();
 
-router.get('/test', userController.test);
-router.post('/add',auth.verifyClient, userController.add);
+router.get("/test", userController.test);
+router.post("/signin", userController.signin);
+router.post("/createAccount", userController.createAccount);
 
 module.exports = router;
