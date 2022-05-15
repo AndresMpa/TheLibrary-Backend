66d15df2e5de8a6aa7f4cf14a887960a0584129b
Andres M. Prieto <andresprieto2703@gmail.com>
2022-05-15 04:34:34 -0500

    Examples
    Adding some extra examples to load data from files

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -235,22 +235,26 @@ module.exports = {
     try {
       const reg = [
         {
-          asset: "https://www.latercera.com/resizer/LTN5QsS3yo6u_oNmub2HNEcv-f4=/900x600/smart/arc-anglerfish-arc2-prod-copesa.s3.amazonaws.com/public/3QOVU47ZLRATTJQREKLUL5BBRA.jpg",
+          asset:
+            "https://www.latercera.com/resizer/LTN5QsS3yo6u_oNmub2HNEcv-f4=/900x600/smart/arc-anglerfish-arc2-prod-copesa.s3.amazonaws.com/public/3QOVU47ZLRATTJQREKLUL5BBRA.jpg",
           title: "Antologia de Terror",
           author: "LoveCraft",
         },
         {
-          asset: "https://www.latercera.com/resizer/LTN5QsS3yo6u_oNmub2HNEcv-f4=/900x600/smart/arc-anglerfish-arc2-prod-copesa.s3.amazonaws.com/public/3QOVU47ZLRATTJQREKLUL5BBRA.jpg",
+          asset:
+            "https://www.latercera.com/resizer/LTN5QsS3yo6u_oNmub2HNEcv-f4=/900x600/smart/arc-anglerfish-arc2-prod-copesa.s3.amazonaws.com/public/3QOVU47ZLRATTJQREKLUL5BBRA.jpg",
           title: "Mago de oz el mundo perdido",
           author: "Oscar de la Frielda",
         },
         {
-          asset: "https://www.latercera.com/resizer/LTN5QsS3yo6u_oNmub2HNEcv-f4=/900x600/smart/arc-anglerfish-arc2-prod-copesa.s3.amazonaws.com/public/3QOVU47ZLRATTJQREKLUL5BBRA.jpg",
+          asset:
+            "https://www.latercera.com/resizer/LTN5QsS3yo6u_oNmub2HNEcv-f4=/900x600/smart/arc-anglerfish-arc2-prod-copesa.s3.amazonaws.com/public/3QOVU47ZLRATTJQREKLUL5BBRA.jpg",
           title: "Como torturar estudiantes",
           author: "Felipe Castillo",
         },
         {
-          asset: "https://www.latercera.com/resizer/LTN5QsS3yo6u_oNmub2HNEcv-f4=/900x600/smart/arc-anglerfish-arc2-prod-copesa.s3.amazonaws.com/public/3QOVU47ZLRATTJQREKLUL5BBRA.jpg",
+          asset:
+            "https://www.latercera.com/resizer/LTN5QsS3yo6u_oNmub2HNEcv-f4=/900x600/smart/arc-anglerfish-arc2-prod-copesa.s3.amazonaws.com/public/3QOVU47ZLRATTJQREKLUL5BBRA.jpg",
           title: "Caminar por las cuerdas de la vida, tomo 1",
           author: "Per Turnum",
         },
@@ -265,15 +269,42 @@ module.exports = {
   },
   getABook: async (req, res, next) => {
     try {
-      const reg = [
+      const top = [
         {
-          asset: "https://librosenlinea.co/wp-content/uploads/2020/01/antologia.jpg",
-          title: "Antología de terror",
-          author: "H. P LoveCraft",
+          asset:
+            "https://librosenlinea.co/wp-content/uploads/2020/01/antologia.jpg",
+          title: "Antologia de Terror",
+          author: "LoveCraft",
           description:
             "Este libro es una guía exhaustiva para adentrarse en los siniestros bosques que alberga el alma del escritor, sus relatos expresan la soledad y la pequeñez de la condición humana en un universo infinito",
         },
+        {
+          asset:
+            "https://www.latercera.com/resizer/LTN5QsS3yo6u_oNmub2HNEcv-f4=/900x600/smart/arc-anglerfish-arc2-prod-copesa.s3.amazonaws.com/public/3QOVU47ZLRATTJQREKLUL5BBRA.jpg",
+          title: "Mago de oz el mundo perdido",
+          author: "Oscar de la Frielda",
+          description:
+            "Un colección de cuentos sobre diferentes historias de piratas muy rica en contenidos",
+        },
+        {
+          asset:
+            "https://www.latercera.com/resizer/LTN5QsS3yo6u_oNmub2HNEcv-f4=/900x600/smart/arc-anglerfish-arc2-prod-copesa.s3.amazonaws.com/public/3QOVU47ZLRATTJQREKLUL5BBRA.jpg",
+          title: "Como torturar estudiantes",
+          author: "Felipe Castillo",
+          description:
+            "En este libro el doctor en tortura de Castillo nos enseña metodos modernos para la tortura psicologica y fisica de estudiantes de todas las edades",
+        },
+        {
+          asset:
+            "https://www.latercera.com/resizer/LTN5QsS3yo6u_oNmub2HNEcv-f4=/900x600/smart/arc-anglerfish-arc2-prod-copesa.s3.amazonaws.com/public/3QOVU47ZLRATTJQREKLUL5BBRA.jpg",
+          title: "Caminar por las cuerdas de la vida, tomo 1",
+          author: "Per Turnum",
+
+          description:
+            "Siendo el primer todo del escritor Turnum este libro nos recibe con una serie de consejos sobre como ser mejores personas realizando un simil con una cuerda de tejer que la madre del doctor Turnum usa",
+        },
       ];
+      const reg = [top[req.body.item]];
       res.status(200).json(reg);
     } catch (e) {
       res.status(500).send({
