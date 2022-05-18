6cbcacbded287c1b374f7b687aab2a701d8f263f
Andres M. Prieto <andresprieto2703@gmail.com>
2022-05-18 03:36:26 -0500

    News
    Making a decent controller

--- routes/news.js
+++ routes/news.js
@@ -1,10 +1,10 @@
-const routerx = require('express-promise-router');
-const newsController = require('../controllers/newsController');
-const auth = require('../middlewares/auth');
+const routerx = require("express-promise-router");
+const newsController = require("../controllers/newsController");
+const auth = require("../middlewares/auth");
 // Instance
 const router = routerx();
 
-router.get('/test', newsController.test);
-router.post('/add',auth.verifyAdmin, newsController.add);
+router.get("/list", newsController.list);
+router.post("/add", auth.verifyAdmin, newsController.add);
 
 module.exports = router;
